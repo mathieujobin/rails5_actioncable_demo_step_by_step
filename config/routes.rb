472658a2db432da73f85b79ed33b60f9278243ca
Mathieu Jobin <mathieu@venuenext.com>
2016-04-30 10:20:00 -0400

    beta1 cable server config, looks unnecessary in beta4

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   root 'rooms#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # During the ActionCable demo on youtube which was running rails 5 beta1
+  # the server line below is uncommented out
+  # but I looks like this is no longer necessary
+  # this snip of code is no longer ship with a beta4 generated application
+  # and it works without.
+
+  # Serve websocket cable requests in-process
+  #mount ActionCable.server => '/cable'
 end
