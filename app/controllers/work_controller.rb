f4d3d2e776ba3bbe1c20daa0d6abc024d16f1bef
Mathieu Jobin <mathieu@venuenext.com>
2017-02-18 06:51:41 -0500

    draft work schedule

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -1,4 +1,19 @@
 class WorkController < ApplicationController
   def timezones
+    nyc = "Eastern Time (US & Canada)"
+#     @mathieu_timezone = ActiveSupport::TimeZone.find_tzinfo(nyc)
+    @mathieu_timezone = 'Tokyo' # ActiveSupport::TimeZone.find_tzinfo("Tokyo")
+    @wakeup_time = 6
+    @work_time = 8..17
+    Time.zone = @mathieu_timezone
+    @foo = Time.zone.local(Time.now.year,Time.now.month,Time.now.day,@wakeup_time,0,0).in_time_zone(visitor_timezones)
+  end
+
+  private
+
+  def visitor_timezones
+    session[:favorite_zone] if ActiveSupport::TimeZone.find_tzinfo(session[:favorite_zone])
+  rescue TZInfo::InvalidTimezoneIdentifier
+    session[:favorite_zone] = nyc
   end
 end
