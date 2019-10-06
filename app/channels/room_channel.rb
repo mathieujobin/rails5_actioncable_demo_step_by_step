586b9883f7758d4dd24b99152ef6122ca84c64f3
Mathieu Jobin <mathieujobin@users.noreply.github.com>
2019-10-07 07:11:59 +0900

    continued Rails 6 - framework files update

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,4 +1,3 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount LetsencryptPlugin::Engine, at: '/'
+  #mount LetsencryptPlugin::Engine, at: '/'
   root 'rooms#welcome'
+  #root 'rooms#show' ?!?
   resources :rooms
-
 
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
