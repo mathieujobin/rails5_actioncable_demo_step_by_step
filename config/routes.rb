692d53569551c0be61a9a5432854123593e822ac
Mathieu Jobin <mathieujobin@users.noreply.github.com>
2019-10-07 07:11:59 +0900

    continued Rails 6 - framework files update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'rooms#show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # During the ActionCable demo on youtube which was running rails 5 beta1
-  # the server line below is uncommented out
-  # but I looks like this is no longer necessary
-  # this snip of code is no longer ship with a beta4 generated application
-  # and it works without.
-
-  # Serve websocket cable requests in-process
-  #mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 end
